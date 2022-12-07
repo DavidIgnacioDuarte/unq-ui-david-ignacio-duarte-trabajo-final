ae92d30fd7a37f948e72313b68e35e390e3fed8e
DavidIgnacioDuarte <davidignacio20@outlook.com>
2022-12-07 02:55:10 -0300

    My signature

--- src/components/HelpOffCanva.jsx
+++ src/components/HelpOffCanva.jsx
@@ -1,4 +1,5 @@
 import { Offcanvas } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
 
 const HelpOffCanva = ({show, onHide}) => {
 
@@ -10,6 +11,11 @@ const HelpOffCanva = ({show, onHide}) => {
             <Offcanvas.Body>
                 A quick and entertaining game to see how much you know about general knowledge. 
                 Just select the difficulty and play!
+                <div style={{position: "absolute", bottom:"15px"}} className="d-flex flex-column align-items-right">
+                    <h4>Made with a lot of dedication by</h4>
+                    <a href="https://github.com/DavidIgnacioDuarte" target="_blank">David Ignacio Duarte <AiFillGithub size="20px"/></a>
+                    
+                </div>
             </Offcanvas.Body>
       </Offcanvas>
     )
